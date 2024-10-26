48ad8f8b8a70a582af3cbdee4d846970f78d422d
Ivan Draganov <idraganov@insincloud.com>
2024-10-26 17:57:27 +0300

    create header and locations section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Locations from './components/Locations/Locations'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <div className="main-content">
+        <Locations />
+      </div>
+      <Footer />
     </>
   )
 }
