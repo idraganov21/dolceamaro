bb8071c50af6511ef38c66ed7682f8de9efd024e
Ivan Draganov <idraganov@insincloud.com>
2024-10-27 20:05:29 +0200

    create brand new navbar + header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import logo from '/logo.png';
-import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaBars, FaClock, FaPhone } from 'react-icons/fa';
+import logo from '/dalogo.png';
+import backgroundImage from '/background.jpg';
+import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaBars } from 'react-icons/fa';
 
 const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -11,73 +12,63 @@ const Navbar = () => {
     };
 
     return (
-        <>
-            <div className={styles.topBar}>
-                <div className={styles.contactInfo}>
-                    <FaMapMarkerAlt className={styles.locationIcon} />
-                    <p>Верига ресторанти Dolce Amaro</p>
-                </div>
-                <div className={styles.socialIcons}>
-                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                        <FaFacebook className={styles.socialIcon} />
-                    </a>
-                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram className={styles.socialIcon} />
-                    </a>
-                </div>
-            </div>
-
+        <header
+            className={styles.header}
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '850px',
+                width: '100%'
+            }}
+        >
             <nav className={styles.navbar}>
                 <div className={styles.navContent}>
-                    <div className={styles.navMenu}>
-                        <p className={styles.navText}>Добрият вкус е винаги на мода!</p>
+                    <div className={styles.leftSection}>
+                        <img src={logo} alt="logo" className={styles.logo} />
                     </div>
-                    <div className={styles.logo}>
-                        <img src={logo} alt="logo" />
-                    </div>
-                    <div className={styles.navMenu}>
-                        <div className={styles.clock}>
-                            <FaClock className={styles.clockIcon} />
-                            <p className={styles.navText}>09:00 – 00:00</p>
+                    <div className={styles.rightSection}>
+                        {/* Линкове за десктоп */}
+                        <div className={styles.desktopMenu}>
+                            <a href="#" className={styles.navItem}>Морска Градина</a>
+                            <a href="#" className={styles.navItem}>Морска Гара</a>
+                            <a href="#" className={styles.navItem}>Меню</a>
+                            <a href="#" className={styles.navItem}>Събития</a>
                         </div>
-                    </div>
+                        {/* Социални икони */}
+                        <div className={styles.socialIcons}>
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <FaFacebook className={styles.socialIcon} />
+                            </a>
+                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                                <FaInstagram className={styles.socialIcon} />
+                            </a>
+                        </div>
 
-                    <div className={styles.hamburger} onClick={toggleMenu}>
-                        <FaBars color="black" />
-                    </div>
+                        {/* Бургер икона за мобилни устройства */}
+                        <div className={styles.hamburger} onClick={toggleMenu}>
+                            <FaBars color="white" />
+                        </div>
 
-                    <div className={`${styles.mobileMenu} ${menuOpen ? styles.active : ''}`}>
-                        <button className={styles.closeButton} onClick={toggleMenu}>X</button>
-                        <a href="#" className={styles.navItem}>Морска Градина</a>
-                        <a href="#" className={styles.navItem}>Морска Гара</a>
-                        <a href="#" className={styles.navItem}>Меню</a>
-                        <a href="#" className={styles.navItem}>Събития</a>
+                        {/* Мобилно меню */}
+                        <div className={`${styles.mobileMenu} ${menuOpen ? styles.active : ''}`}>
+                            <button className={styles.closeButton} onClick={toggleMenu}>X</button>
+                            <a href="#" className={styles.navItem}>Морска Градина</a>
+                            <a href="#" className={styles.navItem}>Морска Гара</a>
+                            <a href="#" className={styles.navItem}>Меню</a>
+                            <a href="#" className={styles.navItem}>Събития</a>
+                        </div>
                     </div>
                 </div>
             </nav>
-            <div className={styles.bottomBar}>
-                <div className={styles.navMenu}>
-                    <a href="https://www.garden.dolceamaro.bg" className={styles.navItem}>Морска Градина</a>
-                    <a href="#" className={styles.navItem}>Морска Гара</a>
-                    <a href="https://dolceamaro.bg/%d0%b4%d0%be%d1%81%d1%82%d0%b0%d0%b2%d0%ba%d0%b0/" className={styles.navItem}>Меню</a>
-                    <a href="https://dolceamaro.bg/%d0%bd%d0%be%d0%b2%d0%b8%d0%bd%d0%b8-dolce-amaro/" className={styles.navItem}>Събития</a>
-                    <a href="#" className={styles.navItem}>
-                        Контакти
-                        <div className={styles.contactDropdown}>
-                            <div className={styles.contactInfo}>
-                                <FaPhone className={styles.contactIcon} />
-                                <p>Долче Амаро Seaport: 0877 622 607</p>
-                            </div>
-                            <div className={styles.contactInfo}>
-                                <FaPhone className={styles.contactIcon} />
-                                <p>Долче Амаро Garden: 0877 622 641</p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+
+            <div className={styles.sloganContainer}>
+                <p className={styles.slogan}>Добрият вкус е винаги на мода!</p>
             </div>
-        </>
+        </header>
     );
 };
 
 export default Navbar;
+
+
