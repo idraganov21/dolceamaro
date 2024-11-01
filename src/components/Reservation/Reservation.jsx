ecf9b26aa93baba7c4a39654c6c8355358d37d47
Ivan Draganov <idraganov@insincloud.com>
2024-11-01 19:15:45 +0200

    create res module

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 import styles from './Reservation.module.css';
 import seaport from '/seaportres.jpg';
 import garden from '/gardenres.jpg';
-import loc from '/loc.png';
-import img1 from '/garden.png';
-import img2 from '/seaport.png';
 
-const Locations = () => {
+const Reservations = () => {
     const [showModal, setShowModal] = useState(false);
+    const [state, handleSubmit] = useForm("xeoqazdb");
 
     const openModal = () => {
         setShowModal(true);
@@ -21,8 +20,8 @@ const Locations = () => {
         <section className={styles.locationsSection}>
             <div className={styles.fLocationBox}>
                 <div className={styles.imageWrapper}>
-                    <img src={seaport} className={styles.sectionImg} alt="dolce amaro" />
-                    <img src={garden} className={styles.overlayImg} alt="overlay" />
+                    <img src={seaport} className={styles.sectionImg} alt="Dolce Amaro" />
+                    <img src={garden} className={styles.overlayImg} alt="Overlay" />
                 </div>
             </div>
             <div className={styles.locationBox}>
@@ -31,7 +30,7 @@ const Locations = () => {
                         <p className={styles.orderTitle}>Резервирай маса!</p>
                     </div>
                     <p className={styles.desc}>
-                    Резервирайте маса в Dolce Amaro Garden или Dolce Amaro Seaport и се насладете на незабравимо кулинарно изживяване. Очакваме Ви в приятната атмосфера на нашите ресторанти
+                        Резервирайте маса в Dolce Amaro Garden или Dolce Amaro Seaport и се насладете на незабравимо кулинарно изживяване. Очакваме Ви в приятната атмосфера на нашите ресторанти.
                     </p>
                     <button className={styles.btn} onClick={openModal}>
                         Резервирай
@@ -39,27 +38,54 @@ const Locations = () => {
                 </div>
             </div>
 
-            {/* Модалният прозорец */}
             {showModal && (
                 <div className={styles.modalOverlay} onClick={closeModal}>
                     <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-                        <p className={styles.modalTitle}>Доставки до дома и офиса!</p>
-                        <div className={styles.modalImages}>
-                            <div className={styles.modalItem}>
-                                <img src={img1} alt="image1" className={styles.modalImage} />
+                        <div className={styles.modalLeft}>
+                            <h2 className={styles.modalTitle}>Резервирай маса</h2>
+                            <form className={styles.reservationForm} onSubmit={handleSubmit}>
                                 <div>
-                                    <button className={styles.btn} onClick={() => window.location.href = 'https://www.dolceamaro.bg/доставка'}>
-                                        Поръчай
-                                    </button>
+                                    <label>
+                                        <input type="text" placeholder='Име и фамилия:' name="name" id="name" required />
+                                    </label>
+                                    <label>
+                                        <input type="email" placeholder='Имейл:' name="email" id="email" required />
+                                    </label>
                                 </div>
+                                <div>
+                                    <label>
+                                        <input type="tel" placeholder='Телефонен номер:' name="phone" id="phone" required />
+                                    </label>
+                                    <label>
+                                        <input type="people" placeholder='Брой хора:' name="people" id="people" required />
+                                    </label>
+                                    <label>
+                                        <select name="location" id="location" required>
+                                            <option disabled>Изберете локация</option>
+                                            <option value="garden">Dolce Amaro Garden</option>
+                                            <option value="seaport">Dolce Amaro Seaport</option>
+                                        </select>
+                                    </label>
+                                </div>
+                                <label>
+                                    <textarea placeholder='Бележка:' name="note" id="note"></textarea>
+                                </label>
+                                <button type="submit" className={styles.submitBtn}>Резервирай</button>
+                            </form>
+                        </div>
+                        <div className={styles.modalRight}>
+                            <h3 className={styles.contactsTitle}>Свържете се с нас</h3>
+                            <div className={styles.contacts}>
+                                <h1 className={styles.contactsTitle}>Dolce Amaro Garden</h1>
+                                <p>Телефон: 087 762 2641</p>
+                                <p>Адрес: Морска градина, до спирка Акациите</p>
+                                <p>Работно време: 11:00 - 00:00</p>
                             </div>
-                            <div className={styles.modalItem}>
-                                <img src={img2} alt="image2" className={styles.modalImage} />
-                                <div>
-                                    <button className={styles.btn} onClick={() => window.location.href = '#'}>
-                                        Поръчай
-                                    </button>
-                                </div>
+                            <div className={styles.contacts}>
+                            <h1 className={styles.contactsTitle}>Dolce Amaro Seaport</h1>
+                                <p>Телефон: 087 762 2607</p>
+                                <p>Адрес: Морска Гара</p>
+                                <p>Работно време: 11:00 - 00:00</p>
                             </div>
                         </div>
                         <button className={styles.closeModalBtn} onClick={closeModal}>Затвори</button>
@@ -70,4 +96,4 @@ const Locations = () => {
     );
 };
 
-export default Locations;
+export default Reservations;
---
+++ src/components/Reservation/ReservationModule.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReservationModule() {
+  return (
+    <div>ReservationModule</div>
+  )
+}
+
+export default ReservationModule;
